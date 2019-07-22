3544b94a4e26bfcda35cfce8cb6577500be44764
silvestrevivo <silvestrevivomillan@gmail.com>
2019-07-23 00:18:31 +0200

    geting a posting data on the data base

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
-const port = 3000;
 
+// Middlewares
+app.use(express.json()); //make posible to read json data from req.body
+
+// URL's
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello World!', app: 'Natours' });
 });
@@ -11,4 +15,38 @@ app.post('/', (req, res) => {
   res.send('You can port to this endpoint...');
 });
 
+// API
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+  // this is data has to be available always, that's why is synchronous
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    // inside the event loop, this has to be asynchronous for non blocking
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      // 201 means created
+      res.status(201).json({
+        status: 'success',
+        data: { tours: newTour },
+      });
+    }
+  );
+});
+
+const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
