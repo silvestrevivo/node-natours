636c2bedfab87a898fed9b126c0382a0c3af8603
silvestrevivo <silvestrevivomillan@gmail.com>
2019-07-26 23:43:38 +0200

    getting single tour based on id

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 // Middlewares
 app.use(express.json()); //make posible to read json data from req.body
 
-// URL's
+//* URL's
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello World!', app: 'Natours' });
 });
@@ -15,12 +15,13 @@ app.post('/', (req, res) => {
   res.send('You can port to this endpoint...');
 });
 
-// API
+//* API
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
   // this is data has to be available always, that's why is synchronous
 );
 
+// GET requests
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -29,6 +30,23 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const tour = tours.find(el => el.id === parseInt(req.params.id, 10));
+
+  // 404 => object not found, not exists in the server
+  if (!tour)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'The tour with the given ID was not found.',
+    });
+
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+});
+
+// POST requests
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
