376c2a2b23a06ad56808c3697a2587f07ec36549
silvestrevivo <silvestrevivomillan@gmail.com>
2019-07-29 16:19:55 +0200

    connecting to data base

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 // Conect the .env file with our app
 dotenv.config({ path: './config.env' });
 
 const app = require('./app.js');
+
+// Connecting to MongoDB
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
 
 //* SERVER
 const port = process.env.PORT;
